44433629a73fb412f874c81d5f3aa0bb73acc24d
Tengyyy <hansmartintomingas@gmail.com>
2024-11-29 05:53:35 +0200

    Frontend structure

---
+++ frontend/src/components/ModeSelect.vue
@@ -0,0 +1,1 @@
+# select between playing mode or teacher/editing mode
---
+++ frontend/src/components/WordList.vue
@@ -0,0 +1,3 @@
+# List of words that should be searched for on the board, could be in the same language as the words on the board or a translation
+
+# In teacher/editor mode, the wordlist should support adding/removing of words and regenerating the table
