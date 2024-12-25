fc5a859111f27cffbdbe075c149902b87a66950b
Tengyyy <hansmartintomingas@gmail.com>
2024-12-25 18:18:09 +0200

    Word search grid

--- frontend/src/components/GameBoard.vue
+++ frontend/src/components/GameBoard.vue
@@ -1,54 +1,42 @@
-# TODO gameboard/grid
-# Need to figure out how to implement the word selection, dragging mechanic
+<script setup>
+import  { computed } from 'vue'
+import GridCell from './GridCell.vue';
 
-E	F	U	U	B	I	A	C	X	G	S	Y	T	B	A
-B	L	O	I	R	N	G	T	L	Z	J	N	I	E	H
-U	N	E	U	G	A	N	J	O	T	J	O	I	L	P
-J	U	S	V	U	I	K	F	P	K	E	I	G	D	W
-G	Õ	H	B	A	T	V	Ä	W	E	Z	P	E	K	K
-G	M	E	I	O	N	O	Õ	Ä	H	K	R	R	I	C
-V	U	L	H	X	Q	T	E	L	J	A	O	D	V	L
-F	W	Z	M	O	N	E	F	B	C	E	K	J	R	T
-W	I	O	O	B	B	R	A	X	G	L	S	Y	A	F
-F	F	W	Q	T	S	U	Y	U	I	K	R	Q	S	K
-B	Z	Y	C	M	H	P	W	M	P	I	P	K	A	C
-B	B	A	S	J	G	F	H	I	D	R	C	R	N	D
-M	T	D	V	A	M	G	R	R	N	J	Q	E	I	S
-H	X	L	M	A	D	U	C	B	L	A	B	E	N	D
-E	V	O	I	K	Q	T	O	A	I	K	S	Y	K	I
+const props = defineProps({
+  grid: {
+    type: Array,
+    required: true
+  },
+  words: {
+    type: Array,
+    required: true
+  }
+})
 
-loomad
+const gridStyle = computed(() => {
+  const columns = props.grid[0]?.length || 0;
 
-elevant		    jääkaru		    jõehobu	
-kaelkirjak		lõvi		    madu	
-ninasarvik		skorpion		tiiger
+  return {
+    display: 'grid',
+    gridTemplateColumns: `repeat(${columns}, 1fr)`,
+    border: '2px solid black',
+    padding: '5px',
+    backgroundColor: '#f9f9f9'
+  }
+})
 
-board = [
-    ['E', 'F', 'U', 'U', 'B', 'I', 'A', 'C', 'X', 'G', 'S',	'Y', 'T', 'B', 'A'],
-    ['B', 'L', 'O', 'I', 'R', 'N', 'G', 'T', 'L', 'Z', 'J',	'N', 'I', 'E', 'H'],
-    ['U', 'N', 'E', 'U', 'G', 'A', 'N', 'J', 'O', 'T', 'J',	'O', 'I', 'L', 'P'],
-    ['J', 'U', 'S', 'V', 'U', 'I', 'K', 'F', 'P', 'K', 'E',	'I', 'G', 'D', 'W'],
-    ['G', 'Õ', 'H', 'B', 'A', 'T', 'V', 'Ä', 'W', 'E', 'Z',	'P', 'E', 'K', 'K'],
-    ['G', 'M', 'E', 'I', 'O', 'N', 'O', 'Õ', 'Ä', 'H', 'K',	'R', 'R', 'I', 'C'],
-    ['V', 'U', 'L', 'H', 'X', 'Q', 'T', 'E', 'L', 'J', 'A',	'O', 'D', 'V', 'L'],
-    ['F', 'W', 'Z', 'M', 'O', 'N', 'E', 'F', 'B', 'C', 'E',	'K', 'J', 'R', 'T'],
-    ['W', 'I', 'O', 'O', 'B', 'B', 'R', 'A', 'X', 'G', 'L',	'S', 'Y', 'A', 'F'],
-    ['F', 'F', 'W', 'Q', 'T', 'S', 'U', 'Y', 'U', 'I', 'K',	'R', 'Q', 'S', 'K'],
-    ['B', 'Z', 'Y', 'C', 'M', 'H', 'P', 'W', 'M', 'P', 'I',	'P', 'K', 'A', 'C'],
-    ['B', 'B', 'A', 'S', 'J', 'G', 'F', 'H', 'I', 'D', 'R',	'C', 'R', 'N', 'D'],
-    ['M', 'T', 'D', 'V', 'A', 'M', 'G', 'R', 'R', 'N', 'J',	'Q', 'E', 'I', 'S'],
-    ['H', 'X', 'L', 'M', 'A', 'D', 'U', 'C', 'B', 'L', 'A',	'B', 'E', 'N', 'D'],
-    ['E', 'V', 'O', 'I', 'K', 'Q', 'T', 'O', 'A', 'I', 'K',	'S', 'Y', 'K', 'I']
-];
+</script>
 
-words = [
-    'elevant',
-    'kaelkirjak',
-    'ninasarvik',
-    'jääkaru',
-    'lõvi',
-    'skorpion',
-    'jõehobu',
-    'madu',
-    'tiiger'
-];
+<template>
+  <div class="grid" :style="gridStyle">
+    <template v-for="(row, rowIndex) in grid" :key="'row-' + rowIndex">
+      <template v-for="(char, colIndex) in row" :key="'cell-' + rowIndex + '-' + colIndex">
+        <GridCell :char="char" :row="rowIndex" :col="colIndex" />
+      </template>
+    </template>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/GridCell.vue
@@ -0,0 +1,38 @@
+<script setup>
+defineProps({
+  row: {
+    type: Number,
+    required: true
+  },
+  col: {
+    type: Number,
+    required: true
+  },
+  char: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="cell">
+    {{ char }}
+  </div>
+</template>
+
+<style scoped>
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Courier New', Courier, monospace; /* Suitable for word search */
+    font-size: 20px; /* Readable font size */
+    font-weight: bold;
+    color: #333; /* Dark text for readability */
+    width: 40px; /* Match gridTemplateColumns */
+    height: 40px; /* Uniform cell height */
+    border: 1px solid #ccc; /* Cell borders */
+    background-color: white; /* Uniform background */
+  }
+</style>
