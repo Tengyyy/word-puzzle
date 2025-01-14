cd7181becc0f3c9be932473acb54dc4915620156
Tengyyy <hansmartintomingas@gmail.com>
2025-01-15 01:17:35 +0200

    Move grid settings to separate component

---
+++ frontend/src/components/GameSettings.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue';
+
+const width = ref(15);
+const height = ref(15);
+const overlap = ref('no-overlap')
+
+const backwardsEnabled = ref(false);
+const diagonalsEnabled = ref(false);
+
+const casing = ref('uppercase');
+</script>
+
+<template>
+  <main>
+    <h4>Mängu sätted</h4>
+    <label for="width-input">Laius:</label><br>
+    <input type="number" id="width-input" name="width-input" v-model="width" /><br><br>
+    <label for="height-input">Kõrgus:</label><br>
+    <input type="number" id="height-input" name="height-input" v-model="height" /><br><br>
+    <label>Sõnade kattumine:</label><br>
+    <input type="radio" id="no-overlap-radio" value="no-overlap" v-model="overlap" />
+    <label for="no-overlap-radio">Sõnad ei kattu</label><br>
+    <input type="radio" id="possible-overlap-radio" value="possible-overlap" v-model="overlap" />
+    <label for="possible-overlap-radio">Sõnad võivad kattuda</label><br>
+    <input type="radio" id="force-overlap-radio" value="force-overlap" v-model="overlap" />
+    <label for="force-overlap-radio">Sõnad kattuvad võimalikult palju</label><br><br>
+    <input type="checkbox" id="backwards-enabled-checkbox" v-model="backwardsEnabled" />
+    <label for="backwards-enabled-checkbox">Sõnad võivad esineda vastupidises suunas</label><br>
+    <input type="checkbox" id="diagonals-enabled-checkbox" v-model="diagonalsEnabled" />
+    <label for="diagonals-enabled-checkbox">Sõnad võivad esineda diagonaalis</label><br><br>
+    <label>Tähtede suurus:</label><br>
+    <input type="radio" id="uppercase-radio" value="uppercase" v-model="casing" />
+    <label for="uppercase-radio">Suurtähed</label><br>
+    <input type="radio" id="lowercase-radio" value="lowercase" v-model="casing" />
+    <label for="lowercase-radio">Väiketähed</label><br><br>
+  </main>
+</template>
--- frontend/src/views/CreateView.vue
+++ frontend/src/views/CreateView.vue
@@ -1,17 +1,16 @@
 <script setup>
+import GameSettings from '@/components/GameSettings.vue';
+import GameBoard from '@/components/GameBoard.vue';
+import WordList from '@/components/WordList.vue';
 import { ref } from 'vue';
 
 const title = ref(null);
-const width = ref(15);
-const height = ref(15);
-const overlap = ref('no-overlap')
 
-const backwardsEnabled = ref(false);
-const diagonalsEnabled = ref(false);
+// should be a openable/closeable component on the top, in the middle left should be the grid, right the word list and input box, 
+// bottom the button regenerate grid, and share/get code/print. 
+// title input should be moved outside of the grid control component, should be below that, 
+// the topic input for generation should be on top of the word list
 
-const casing = ref('upper');
-
-//TODO: move the controls/settings to a separate component
 
 //TODO: for the word list a toggle to alphabetize the word list
 //TODO: grid/board, wordlist with input to enter more words, button to regenereta grid
@@ -22,19 +21,10 @@ const casing = ref('upper');
 
 <template>
   <main>
+    <GameSettings />
     <label for="title-input">Pealkiri:</label><br>
     <input type="text" id="title-input" name="title-input" v-model="title" /><br><br>
-    <label for="width-input">Laius:</label><br>
-    <input type="number" id="width-input" name="width-input" v-model="width" /><br><br>
-    <label for="height-input">Kõrgus:</label><br>
-    <input type="number" id="height-input" name="height-input" v-model="height" /><br><br>
-    <label>Sõnade kattumine:</label><br>
-    <input type="radio" id="no-overlap-radio" value="no-overlap" v-model="overlap" />
-    <label for="no-overlap-radio">Sõnad ei kattu</label><br>
-    <input type="radio" id="possible-overlap-radio" value="possible-overlap" v-model="overlap" />
-    <label for="possible-overlap-radio">Sõnad võivad kattuda</label><br>
-    <input type="radio" id="force-overlap-radio" value="force-overlap" v-model="overlap" />
-    <label for="force-overlap-radio">Sõnad kattuvad võimalikult palju</label><br>
-
+    <GameBoard />
+    <WordList />
   </main>
 </template>
