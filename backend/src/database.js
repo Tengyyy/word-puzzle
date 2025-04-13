d0e41e1779d5cf1ba01c2ed0e4dd491139897144
Tengyyy <hansmartintomingas@gmail.com>
2025-04-13 04:51:05 +0300

    try different uuid generator

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,5 +1,6 @@
 import pkg from "pg";
 import logger from './logger.js';
+import { v4 as uuidv4 } from 'uuid';
 
 
 const { Pool } = pkg;
@@ -25,14 +26,7 @@ const execute = async (query) => {
 const createTablesQuery = `
     CREATE TABLE IF NOT EXISTS "games"
     (
-        id
-        uuid
-        PRIMARY
-        KEY
-        DEFAULT
-        gen_random_uuid
-    (
-    ),
+        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
         topic TEXT NOT NULL,
         title TEXT NOT NULL,
         grid TEXT[][] NOT NULL,
